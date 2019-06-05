542922701d69c529609f0c3b8c7568e5c1b9b40d
Juha Riihelä <juha.riihela@meteoriitti.com>
2019-06-05 11:28:34 +0300

    personapi v2

---
+++ build/precache-manifest.b9cca2d35067307665a813eb0409d9dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "605511ad9ce920bc52c3f695c7b2624c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d249043e1d7d02291e89",
+    "url": "/static/css/main.f149f911.chunk.css"
+  },
+  {
+    "revision": "6a19a0beea039a280f26",
+    "url": "/static/js/2.3c62b441.chunk.js"
+  },
+  {
+    "revision": "d249043e1d7d02291e89",
+    "url": "/static/js/main.e930ebd4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const Phone = require('./models/phone')
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
-//app.use(express.static('build'))
+app.use(express.static('build'))
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) });
 var loggerFormat = ':method :url :status :res[content-length] - :response-time ms :body';
